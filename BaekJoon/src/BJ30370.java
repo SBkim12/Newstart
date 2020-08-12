ba96c38637da0f836a3909cf70bfcddd654a3985
SBkim12 <mar5924@naver.com>
2020-08-12 20:12:45 +0900

    8/12

---
+++ BaekJoon/src/BJ30370.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class BJ30370 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str;
+		while(sc.hasNextLine()) {
+			str =sc.nextLine();
+			if(str.length()==0) break;
+			System.out.println(str);
+		}
+		sc.close();
+	}
+
+}
